5bbc27422e406d134d253a0ab6bc77def88ec059
Duy Nguyen <duynguyen02.dev@gmail.com>
2024-09-29 01:14:48 +0700

    inherit additional upload services

--- Sources/SwiftReq/Service.swift
+++ Sources/SwiftReq/Service.swift
@@ -17,63 +17,68 @@ public class Response<T: Decodable> {
     let error: String?
 }
 
-public class ServiceBuilder<T: Decodable> {
+public class Service<T: Decodable> {
     public init(swiftReq: SwiftReq, endpoint: Endpoint) {
         self.swiftReq = swiftReq
         self.endpoint = endpoint
     }
 
-    private let swiftReq: SwiftReq
-    private let endpoint: Endpoint
-    private var method: HTTPMethod = .get
-    private var headers: [String: String] = [:]
-    private var parameters: [String: String] = [:]
+    fileprivate let swiftReq: SwiftReq
+    fileprivate let endpoint: Endpoint
+    fileprivate var queries: [Query] = []
+    fileprivate var method: HTTPMethod = .get
+    fileprivate var headers: [String: String] = [:]
 
     @discardableResult
-    public func setMethod(_ method: HTTPMethod) -> ServiceBuilder {
+    public func setMethod(_ method: HTTPMethod) -> Self {
         self.method = method
         return self
     }
 
     @discardableResult
-    public func setHeaders(_ headers: [String: String]) -> ServiceBuilder {
+    public func setQueries(_ queries: [Query]) -> Self {
+        self.queries = queries
+        return self
+    }
+
+    @discardableResult
+    public func addQuery(_ query: Query) -> Self {
+        self.queries.append(query)
+        return self
+    }
+
+    @discardableResult
+    public func setHeaders(_ headers: [String: String]) -> Self {
         self.headers = headers
         return self
     }
 
     @discardableResult
-    public func addHeader(key: String, value: String) -> ServiceBuilder {
+    public func addHeader(key: String, value: String) -> Self {
         self.headers[key] = value
         return self
     }
 
-    @discardableResult
-    public func setParameters(_ parameters: [String: String]) -> ServiceBuilder {
-        self.parameters = parameters
-        return self
+    fileprivate func getURLWithQueries(url: URL) -> URL {
+        if queries.count == 0 {
+            return url
+        }
+        var urlComponents: URLComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        let queryParams: [URLQueryItem] = queries.map { (key: String, value: String) in
+            URLQueryItem(name: key, value: value)
+        }
+        urlComponents.queryItems = queryParams
+        return urlComponents.url!
     }
 
-    @discardableResult
-    public func addParameter(key: String, value: String) -> ServiceBuilder {
-        self.parameters[key] = value
-        return self
-    }
-
-    private func getURLWithEndpoint() -> URL {
+    fileprivate func getURLWithEndpointAndQueries() -> URL {
         var url: URL = swiftReq.host
         url.appendPathComponent(self.endpoint)
+        url = getURLWithQueries(url: url)
         return url
     }
 
-    private func getAllParameters() -> Parameters {
-        var requestParams: Parameters = parameters
-        requestParams.merge(swiftReq.parameters) { current, _ in
-            current
-        }
-        return parameters
-    }
-
-    private func getAllHeaders() -> Headers {
+    fileprivate func getAllHeaders() -> Headers {
         var requestHeaders: Headers = headers
         requestHeaders.merge(swiftReq.headers) { current, _ in
             current
@@ -81,36 +86,36 @@ public class ServiceBuilder<T: Decodable> {
         return requestHeaders
     }
 
-    private func build() -> DataRequest {
-        let url: URL = getURLWithEndpoint()
-        let parameters: Parameters = getAllParameters()
+    public func build() -> DataRequest {
+        let url: URL = getURLWithEndpointAndQueries()
         let headers: Headers = getAllHeaders()
 
         return Alamofire.AF.request(
             url,
             method: method,
-            parameters: parameters,
             headers: HTTPHeaders(headers),
             interceptor: swiftReq.requestInterceptor
         ) { request in
             request.timeoutInterval = self.swiftReq.timeout
         }
+
     }
 
-    func request() async throws -> T {
+    public func request() async throws -> T {
         return try await build().serializingDecodable(T.self).value
     }
 
-    func response() async throws -> Response<T> {
+    public func response() async throws -> Response<T> {
         let response: DataResponse<T, AFError> = await build().serializingDecodable(T.self).response
 
         var isSuccessful: Bool = false
-        if let statusCode: Int = response.response?.statusCode{
+        if let statusCode: Int = response.response?.statusCode {
             isSuccessful = (200..<300).contains(statusCode)
         }
 
         var error: String? = nil
-        if let data: Data = response.data{
+        if response.data != nil && (!isSuccessful || response.value == nil){
+            let data: Data = response.data!
             error = String(decoding: data, as: UTF8.self)
         }
 
@@ -124,33 +129,117 @@ public class ServiceBuilder<T: Decodable> {
 
 }
 
-class Method<T: Decodable>: ServiceBuilder<T> {
-    init(swiftReq: SwiftReq, endpoint: Endpoint, method: HTTPMethod) {
-        super.init(swiftReq: swiftReq, endpoint: endpoint)
-        setMethod(method)
+
+public class URLEncodedFormDataService<T: Decodable>: Service<T> {
+
+    fileprivate var parameters: Parameters? = nil
+    fileprivate var encoder: URLEncodedFormParameterEncoder = URLEncodedFormParameterEncoder.default
+
+    @discardableResult
+    public func setParameters(_ parameters: Parameters) -> Self {
+        self.parameters = parameters
+        return self
+    }
+
+    @discardableResult
+    public func addParameter(_ parameter: (key: String, value: String)) -> Self {
+        if self.parameters == nil {
+            self.parameters = [:]
+        }
+        self.parameters?[parameter.key] = parameter.value
+        return self
+    }
+
+    @discardableResult
+    public func setEncoder(_ encoder: URLEncodedFormParameterEncoder) -> Self {
+        self.encoder = encoder
+        return self
+    }
+
+    public override func build() -> DataRequest {
+        let url: URL = getURLWithEndpointAndQueries()
+        let headers: Headers = getAllHeaders()
+
+        return Alamofire.AF.request(
+            url,
+            method: method,
+            parameters: parameters,
+            encoder: encoder,
+            headers: HTTPHeaders(headers),
+            interceptor: swiftReq.requestInterceptor
+        ) { request in
+            request.timeoutInterval = self.swiftReq.timeout
+        }
     }
 }
 
-class Get<T: Decodable>: Method<T> {
-    init(swiftReq: SwiftReq, endpoint: Endpoint) {
-        super.init(swiftReq: swiftReq, endpoint: endpoint, method: .get)
+public class MultipartFormDataService<T: Decodable>: Service<T> {
+
+    fileprivate var formFields: FormFields = [:]
+
+    @discardableResult
+    public func setFormFields(_ formFields: FormFields) -> Self {
+        self.formFields = formFields
+        return self
+    }
+
+    @discardableResult
+    public func addFormField(_ formField: (String, Data)) -> Self {
+        self.formFields[formField.0] = formField.1
+        return self
+    }
+
+    public override func build() -> DataRequest {
+        let url: URL = getURLWithEndpointAndQueries()
+        let headers: Headers = getAllHeaders()
+
+        return Alamofire.AF.upload(
+            multipartFormData: { fd in
+                self.formFields.forEach { fd.append($1, withName: $0) }
+            }, to: url,
+            method: method,
+            headers: HTTPHeaders(headers),
+            interceptor: swiftReq.requestInterceptor
+        ) { request in
+            request.timeoutInterval = self.swiftReq.timeout
+        }
     }
 }
 
-class Post<T: Decodable>: Method<T> {
-    init(swiftReq: SwiftReq, endpoint: Endpoint) {
-        super.init(swiftReq: swiftReq, endpoint: endpoint, method: .post)
-    }
-}
 
-class Put<T: Decodable>: Method<T> {
-    init(swiftReq: SwiftReq, endpoint: Endpoint) {
-        super.init(swiftReq: swiftReq, endpoint: endpoint, method: .put)
-    }
-}
+public class JSONBodyService<T: Decodable, E: Encodable>: Service<T> {
+    fileprivate var body: E? = nil
+    fileprivate var encoder: JSONParameterEncoder = JSONParameterEncoder.default
 
-class Delete<T: Decodable>: Method<T> {
-    init(swiftReq: SwiftReq, endpoint: Endpoint) {
-        super.init(swiftReq: swiftReq, endpoint: endpoint, method: .delete)
+    @discardableResult
+    public func setBody(_ body: E) -> Self {
+        self.body = body
+        return self
+    }
+
+    @discardableResult
+    public func setEncoder(_ encoder: JSONParameterEncoder) -> Self {
+        self.encoder = encoder
+        return self
+    }
+
+    public override func build() -> DataRequest {
+        let url: URL = getURLWithEndpointAndQueries()
+        let headers: Headers = getAllHeaders()
+        
+        guard let body = self.body else {
+            fatalError("Body must be set before calling build().")
+        }
+
+        return Alamofire.AF.request(
+            url,
+            method: method,
+            parameters: body,
+            encoder: self.encoder,
+            headers: HTTPHeaders(headers),
+            interceptor: swiftReq.requestInterceptor
+        ) { request in
+            request.timeoutInterval = self.swiftReq.timeout
+        }
     }
 }
