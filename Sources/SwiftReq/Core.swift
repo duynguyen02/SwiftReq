2e1216abbd776596874ed0f2b63f79d1a44817b7
Duy Nguyen <duynguyen02.dev@gmail.com>
2024-09-29 01:12:05 +0700

    add Query, FormFields typealias

--- Sources/SwiftReq/Core.swift
+++ Sources/SwiftReq/Core.swift
@@ -1,3 +1,7 @@
+import Foundation
+
 public typealias Endpoint = String
+public typealias Query = (key: String,value: String)
 public typealias Parameters = [String: String]
 public typealias Headers = [String: String]
+public typealias FormFields = [String: Data]
